c794f46d4adaff11d7440c2b6c8d6df8d94f58a0
Fiona O'Mahoney <fiona@wantsa.com>
2015-04-13 13:47:32 +0100

    removing ability to take photo from web

--- app/assets/javascripts/mobile/photo_capture.js
+++ app/assets/javascripts/mobile/photo_capture.js
@@ -1,20 +1,5 @@
 $(function() {
 
-    var video = document.querySelector('video#getUserMedia');
-    var canvas = document.querySelector('#canvas');
-    var snapshot = document.querySelector('#display_photo');
-    var localMediaStream = null;
-
-    turnCameraOn(video, canvas, snapshot, $(this));
-
-    $('#take_photo_button').click(function () {
-        // Turn on camera
-        if ($(this).data('camera-state') == 'on')
-            takePhoto(video, canvas, snapshot);
-        else
-            turnCameraOn(video, canvas, snapshot, $(this));
-    });
-
     $('#photo_button').click(function () {
         $('#message_photo').trigger('click');
         return false;
@@ -33,48 +18,3 @@ $(function() {
     });
 
 });
-
-function turnCameraOn(video, canvas, snapshot, captureButton) {
-    var ctx = canvas.getContext('2d');
-
-    captureButton.disabled = true;
-
-    window.URL = window.URL || window.webkitURL;
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-        navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-    var constraints = {
-        video: true
-    }
-
-    var successCallback = function (mediaStream) {
-        video.src = (window.URL && window.URL.createObjectURL(mediaStream)) || mediaStream;
-        video.addEventListener("loadedmetadata", function (e) {
-            video.style.display = "block";
-            localMediaStream = mediaStream;
-            captureButton.data('camera-state', 'on');
-        });
-    };
-
-    var errorCallback = function () {
-        console.log("failure to get media");
-    };
-
-    if (navigator.getUserMedia) {
-        navigator.getUserMedia(constraints, successCallback, errorCallback);
-    } else {
-        console.log("getUserMedia not supported");
-    }
-}
-
-function takePhoto(video, canvas, snapshot)
-{
-    //if (localMediaStream)
-     //   localMediaStream.stop();
-    var ctx = canvas.getContext('2d');
-    ctx.drawImage(video, 0, 0, 198, 240);
-    snapshot.src = canvas.toDataURL('image/webp');
-    //video.style.display = "none";
-    snapshot.style.display = "block";
-    $.mobile.changePage('#page_two', {'transition' : 'slide'});
-}
