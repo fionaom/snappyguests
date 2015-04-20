e12a1207b0b298fc84ab91c868cbb0a8fe342c5b
Fiona O'Mahoney <fiona@wantsa.com>
2015-04-20 14:33:12 +0100

    checking in aspect ratio

--- app/assets/javascripts/mobile/photo_capture.js
+++ app/assets/javascripts/mobile/photo_capture.js
@@ -20,26 +20,34 @@ $(function() {
                 var snapshot = $('#display_photo');
 
                 var maxWidth = MAX_WIDTH; // Max width for the image
-                var minHeight = MAX_HEIGHT;    // Max height for the image
+                var maxHeight = MAX_HEIGHT;    // Max height for the image
                 var ratio = 0;  // Used for aspect ratio
-                var width = $(this).width();    // Current image width
-                var height = $(this).height();  // Current image height
+                var width = this.width;    // Current image width
+                var height = this.height;  // Current image height
 
-                if(width > maxWidth){
-                    ratio = maxWidth / width;   // get ratio for scaling image
-                    snapshot.css("width", maxWidth); // Set new width
-                    snapshot.css("height", height * ratio);  // Scale height based on ratio
-                    height = height * ratio;    // Reset height to match scaled image
-                    width = width * ratio;    // Reset width to match scaled image
+                if (width > height)
+                {
+                    if(width > maxWidth)
+                    {
+                        ratio = maxWidth / width;   // get ratio for scaling image
+                        height = height * ratio;    // Reset height to match scaled image
+                        width = width * ratio;    // Reset width to match scaled image
+                        snapshot.css("width", height); // Set new width
+                        snapshot.css("height", width);  // Scale height based on ratio
+                    }
+                }
+                else
+                {
+                    if(height > maxHeight)
+                    {
+                        ratio = maxHeight / height;   // get ratio for scaling image
+                        height = height * ratio;    // Reset height to match scaled image
+                        width = width * ratio;    // Reset width to match scaled image
+                        snapshot.css("width", width); // Set new width
+                        snapshot.css("height", height);  // Scale height based on ratio
+                    }
                 }
 
-                // Check if current height is larger than max
-                if(height < minHeight){
-                    ratio = minHeight / height; // get ratio for scaling image
-                    snapshot.css("height", minHeight);   // Set new height
-                    snapshot.css("width", width * ratio);    // Scale width based on ratio
-                    width = width * ratio;    // Reset width to match scaled image
-                }
                 snapshot.attr('src', e.target.result).show();
                 $.mobile.changePage('#page_two', {'transition' : 'slide'});
 
