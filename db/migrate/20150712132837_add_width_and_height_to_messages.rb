186eec560ae9d55998274d9341231cb5a788d982
Fiona O'Mahoney <fiona@wantsa.com>
2015-07-22 22:12:30 +0100

    Fixing incorrect migration

--- db/migrate/20150712132837_add_width_and_height_to_messages.rb
+++ db/migrate/20150712132837_add_width_and_height_to_messages.rb
@@ -4,7 +4,7 @@ class AddWidthAndHeightToMessages < ActiveRecord::Migration
     add_column :messages, :photo_height, :integer
 
     Message.all.each do |msg|
-      geometry = Paperclip::Geometry.from_file(msg.photo)
+      geometry = Paperclip::Geometry.from_file(msg.photo.path(:original))
       msg.photo_width = geometry.width.to_i
       msg.photo_height = geometry.height.to_i
       msg.save!
